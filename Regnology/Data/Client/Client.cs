c9b1af8ba017ed81cd8bea51630281c97d5bca7f
Alex-Sirbu2898 <sirbu.alex2898@gmail.com>
2023-06-01 20:53:03 +0300

    Added filtering

---
+++ Regnology/Data/Client/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Regnology.Data
+{
+    public class Client
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CliientId { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ Regnology/Data/Project/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Regnology.Data
+{
+    public class Project
+    {
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateTime Deadline { get; set; }
+
+    public string ClientName { get; set; }
+
+    public virtual IList<Employee> Employee { get; set; }
+
+    public virtual Client Client { get; set; }
+    }
+}
